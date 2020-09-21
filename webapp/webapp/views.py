c21313553cea5b40f2ce79d0c455dee524a8b4dc
Quang Vu <quangbgt@gmail.com>
2020-09-21 14:03:42 +0700

    update frontend - default homepage

--- webapp/webapp/views.py
+++ webapp/webapp/views.py
@@ -1,4 +1,4 @@
 from django.http import  HttpResponse
 
 def index(request):
-    return HttpResponse('<h1>Welcome to AGoodPosition.Com</h1><hr>');
+    return HttpResponse('<h1>www.priceaction.ai - homepage</h1><hr>');
